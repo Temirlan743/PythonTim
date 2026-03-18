2b51167c6d8fa04f46bc7bde03134f12d3578d59
Temirlan <temirlan@MacBook-Pro-Temirlan.local>
2026-03-18 16:04:20 +0600

    fix2

--- pages/homePage.py
+++ pages/homePage.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class HomePage:
@@ -7,18 +8,22 @@ class HomePage:
         self.browser = browser
 
     def open(self):
-       self.browser.get("https://demoblaze.com/index.html")
+        wait = WebDriverWait(self.browser, 10)
+        self.browser.get("https://demoblaze.com/index.html")
 
 
 
     def click_galaxy_s6(self):
+        wait = WebDriverWait(self.browser, 10)
         galaxy_s6 = self.browser.find_element(By.XPATH,'//a[text()="Samsung galaxy s6"]')
         galaxy_s6.click()
 
     def check_products_count(self, count):
+         wait = WebDriverWait(self.browser, 10)
          monitors = self.browser.find_elements(By.XPATH, '//div[@class="card h-100"]')
          assert len(monitors) == count
 
     def click_monitor(self):
+        wait = WebDriverWait(self.browser, 10)
         monitor_link = self.browser.find_element(By.CSS_SELECTOR, '''[onclick="byCat('monitor')"]''')
         monitor_link.click()
